0cdba4fe0e2911d7fa50b378e632e40c0a86eb8e
Matthew Su <matthewsu@college.harvard.edu>
2023-05-30 23:57:23 -0500

    Dark mode toggling and recognizing sys default
    Added dark mode toggle which recognizes the system OS setting as default

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import { type Config } from "tailwindcss";
 
 export default {
-  darkMode: "media",
+  darkMode: ["class"],
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     container: {
